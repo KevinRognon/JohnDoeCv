0063d115f0fc10cb9a003cf36f7a4bb01fe6b40a
Kevin Rognon <rognonk@gmail.com>
2023-06-13 19:36:42 +0200

    Première section WIP

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,14 +8,19 @@ export default function Navbar () {
 
     return (
         <>
-            <header>
+            <header className="position-absolute vw-100 z-3">
                 <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
 
                     <div className="container-fluid">
 
                         <h1 className="navbar-brand ms-5">JOHN DOE</h1>
+                        <button class="navbar-toggler me-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            
+                            <span class="navbar-toggler-icon"></span>
 
-                        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                        </button>
+
+                        <div className="collapse navbar-collapse justify-content-end " id="navbarNav">
 
                             <ul className="navbar-nav gap-3 me-5">
                                 <NavLink className="nav-link" to="/">Accueil</NavLink>
@@ -27,11 +32,6 @@ export default function Navbar () {
 
                         </div>
 
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            
-                            <span class="navbar-toggler-icon"></span>
-
-                        </button>
                     </div>
                 </nav>
             </header>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,18 @@
 
-
 export default function Home () {
 
 
     return (
-        <h1>Test</h1>
+        <section className="section-home d-flex justify-content-center align-items-center container-fluid">
+            <div className="d-flex flex-column gap-5">
+
+                <h1 className="titre-home-section text-light">Bonjour, je suis John Doe</h1>
+
+                <h2 className="titremetier-home-section text-light">Développeur web full stack</h2>
+
+                <button type="button" className="btn btn-primary bouton-savoir"><a href="#a-propos">En savoir plus</a></button>
+
+            </div>
+        </section>
     )
 }
